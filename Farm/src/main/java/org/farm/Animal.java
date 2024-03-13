ae9baec021c99c104daca1da7d6ad8403b5cdcf8
Valeria <152556287+Valeria6845@users.noreply.github.com>
2024-03-13 23:05:17 +0200

    Add files via upload

---
+++ Farm/src/main/java/org/farm/Animal.java
@@ -0,0 +1,13 @@
+package org.farm;
+
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+
+public class Animal {
+    private int id;
+    public int productCollection() {
+        return 0;
+    }
+}
